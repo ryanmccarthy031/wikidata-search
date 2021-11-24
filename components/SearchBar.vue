d09ae99bb9c844a910197813636c0e019597c987
Ryan McCarthy <ryanmccarthy031@gmail.com>
2021-11-24 00:03:27 +0000

    Initial commit

---
+++ components/SearchBar.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <v-autocomplete
+      v-model="selected"
+      :items="items"
+      :loading="isLoading"
+      :search-input.sync="search"
+      color="white"
+      item-text="description"
+      item-value="label"
+      label="WikiData Items"
+      placeholder="Start typing to Search"
+      prepend-icon="mdi-database-search"
+      no-filter
+      return-object>
+      <template v-slot:selection="data">
+        <div>{{ data.item.label }}</div>
+      </template>
+
+      <template 
+        cols="4"
+        v-slot:item="data">
+        <template v-if="typeof data.item==='object'">
+          <v-list-item-content>
+            <v-list-item-title>{{ truncate(data.item.label) }}</v-list-item-title>
+            <v-list-item-subtitle>{{ truncate(data.item.description) }}</v-list-item-subtitle>
+          </v-list-item-content>
+        </template>
+      </template>
+
+      <template 
+        v-slot:append-item>
+        <div>
+          <div 
+            v-if="items.length && searchContinue!==undefined"
+            class="d-flex flex-row-reverse">
+            <v-fab-transition>
+              <v-btn
+                @click.prevent.stop="fetchData(search, true)"
+                class="mr-2"
+                color="pink"
+                fab
+                dark
+                small
+                right
+              >
+                <v-icon>mdi-plus</v-icon>
+              </v-btn>
+            </v-fab-transition>  
+          </div>
+          <div
+            class="pa-3"
+            v-if="selected.label || search">
+            <a :href="`https://www.wikidata.org/w/index.php?search=${search}&title=Special%3ASearch&fulltext=1&ns0=1&ns120=1`">Search for pages containing <em>{{ truncate(selected.label) || search }}</em></a>
+          </div>
+        </div>
+      </template>
+    </v-autocomplete>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      items: [],
+      isLoading: false,
+      model: {},
+      search: null,
+      searchContinue: 0,
+      fieldLength: 60,
+    }),
+    computed: {
+      selected: {
+        set (val) {
+          this.model=val
+          this.$emit('selected', val)
+        },
+        get () {
+          return this.model
+        },
+      },
+    },
+    methods: {
+      // Truncate strings and keep the search options window to a manageable length
+      truncate(str) {
+        if (!str) return ''
+        return (str.length > this.fieldLength) ? str.substr(0, this.fieldLength-1) + '...' : str;
+      },
+
+      fetchData (val, add) {
+        // If there is no search term, reset query params
+        if (!val) {
+          this.items=[]
+          this.searchContinue=0
+          return
+        }
+        // Items have already been requested
+        this.isLoading = true
+
+        // Lazily load input items
+        fetch(`https://www.wikidata.org/w/api.php?action=wbsearchentities&uselang=en&language=en&format=json&origin=*&continue=${this.searchContinue}&search=${val}`)
+          .then(res => res.json())
+          .then(res => {
+            // This sets the value for further results. Will return undefined if the list is exhausted
+            this.searchContinue = res['search-continue']
+            // If this is a new search, replace the existing items, but if it's adding to the existing results, then concat
+            this.items = add ? this.items.concat(res.search) : res.search
+          })
+          .catch(err => {
+            console.log(err)
+          })
+          .finally(() => (this.isLoading = false))
+
+      },
+    },
+    watch: {
+      search (val) {
+        this.fetchData(val)
+      },
+    },
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <v-card>
+        <v-card-title class="headline">
+          Enter a search term
+        </v-card-title>
+        <v-card-text>
+          <SearchBar 
+            @selected="value=$event"/>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            :disabled="!value"
+            nuxt
+            :href="value.url"
+          >
+            <v-icon>mdi-wikipedia</v-icon>
+            Learn More
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import SearchBar from '@/components/SearchBar.vue'
+
+export default {
+  data () {
+    return {
+      value: '',
+    }
+  },
+  components: {
+    SearchBar,
+  },
+}
+</script>
